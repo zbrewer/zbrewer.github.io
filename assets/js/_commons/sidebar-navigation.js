4de1fd78ac91bc59608bae85f734a1f11f278f71
Zach Brewer <zbrewer02@gmail.com>
2021-03-14 21:37:15 -0600

    Changes the sidebar navigation to be configured with the _data/sidebar_navigation.yml file and begins adding support for expanding folders. This includes adding support for re-minimizing the JavaScript with Gulp.

---
+++ assets/js/_commons/sidebar-navigation.js
@@ -0,0 +1,26 @@
+/*
+ * The expand/close effect for the sidebar navigation.
+ */
+
+$(function() {
+  const childPrefix = "n_";
+  const parentPrefix = "f_";
+  const collapse = $(".collapse");
+
+  /* close up top-category */
+  collapse.on("hide.bs.collapse", function () { /* Bootstrap collapse events. */
+    const parentId = parentPrefix + $(this).attr("id").substring(childPrefix.length);
+    if (parentId) {
+      $(`#${parentId} i.fas`).removeClass("sidebar-navigation-rotate");
+    }
+  });
+
+  /* expand the top category */
+  collapse.on("show.bs.collapse", function() {
+    const parentId = parentPrefix + $(this).attr("id").substring(childPrefix.length);
+    if (parentId) {
+      $(`#${parentId} i.fas`).addClass("sidebar-navigation-rotate");
+    }
+  });
+
+});
