6532f5aabfada30b296676a34e81d98325e64dbf
Zach <zbrewer02@gmail.com>
2021-03-21 18:40:50 -0600

    Dynamically sets the height of the sidebar based on the expanded nav sub-folders to keep the sidebar height correct on shorter displays.

--- assets/js/_commons/sidebar-navigation.js
+++ assets/js/_commons/sidebar-navigation.js
@@ -7,21 +7,27 @@ $(function() {
   const parentPrefix = "f_";
   const collapse = $(".collapse");
 
-  /* close up top-category */
+  const buildTabCount = document.documentElement.style.getPropertyValue("--plane-build-tab-count'");
+
+  /* close nav sub-list */
   collapse.on("hide.bs.collapse", function () { /* Bootstrap collapse events. */
     const parentId = parentPrefix + $(this).attr("id").substring(childPrefix.length);
     const currentPrefix = $(this).attr("id").substring(0, childPrefix.length);
     if (parentId && currentPrefix === childPrefix) {
       $(`#${parentId} i.nav-arrow`).removeClass("sidebar-navigation-rotate");
+      const tabCount = document.documentElement.style.getPropertyValue("--tab-count'");
+      document.documentElement.style.setProperty("--tab-count", tabCount - buildTabCount);
     }
   });
 
-  /* expand the top category */
+  /* expand nav sub-list */
   collapse.on("show.bs.collapse", function() {
     const parentId = parentPrefix + $(this).attr("id").substring(childPrefix.length);
     const currentPrefix = $(this).attr("id").substring(0, childPrefix.length);
     if (parentId && currentPrefix === childPrefix) {
       $(`#${parentId} i.nav-arrow`).addClass("sidebar-navigation-rotate");
+      const tabCount = document.documentElement.style.getPropertyValue("--tab-count'");
+      document.documentElement.style.setProperty("--tab-count", tabCount + buildTabCount);
     }
   });
 
